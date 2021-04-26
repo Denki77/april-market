5e886f86da640f74b83bf06e478d0aa4526ddc9c
Denis_Fomenkov <tgimrv@ya.ru>
2021-04-26 22:50:59 +0300

    Initial commit

---
+++ src/test/java/ru/geekbrains/aprilmarket/AprilMarketApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.aprilmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprilMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+
+}
